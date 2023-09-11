93cadc5ba2171d95198af3fbb85ea7bc60c421a9
Tejas Kashid <Tejas.Kashid@sportzinteractive.com>
2023-09-11 13:48:07 +0530

    Sports Management System

---
+++ SportsMatchManagementSystem/MatchManagement.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsMatchManagementSystem
+{
+    internal class MatchManagement
+    {
+        private List<MatchDetails> matches = new List<MatchDetails>();
+
+        public void AddMatch(MatchDetails match)
+        {
+            // Validate match properties before adding
+            if (IsValidMatch(match))
+            {
+                matches.Add(match);
+                Console.WriteLine("Match added successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Match validation failed. Match not added.");
+            }
+        }
+
+        public void DisplayAllMatches()
+        {
+            Console.WriteLine("List of matches:");
+            foreach (var match in matches)
+            {
+                DisplayMatch(match);
+            }
+        }
+
+        public void DisplayMatchById(int matchId)
+        {
+            var match = matches.FirstOrDefault(m => m.MatchId == matchId);
+            if (match != null)
+            {
+                DisplayMatch(match);
+            }
+            else
+            {
+                Console.WriteLine("Match not found with the given ID.");
+            }
+        }
+
+        public void UpdateMatchScore(int matchId, int homeTeamScore, int awayTeamScore)
+        {
+            var match = matches.FirstOrDefault(m => m.MatchId == matchId);
+            if (match != null)
+            {
+                match.HomeTeamScore = homeTeamScore;
+                match.AwayTeamScore = awayTeamScore;
+                Console.WriteLine("Match scores updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Match not found with the given ID. Score updation failed.");
+            }
+        }
+
+        public void RemoveMatch(int matchId)
+        {
+            var match = matches.FirstOrDefault(m => m.MatchId == matchId);
+            if (match != null)
+            {
+                matches.Remove(match);
+                Console.WriteLine("Match removed successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Match not found for given ID.");
+            }
+        }
+
+        private bool IsValidMatch(MatchDetails match)
+        {
+            // Implement validation logic based on the criteria mentioned
+            // You can add additional checks as needed
+            if (match.MatchId <= 0 || matches.Any(m => m.MatchId == match.MatchId))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(match.Sport))
+                return false;
+
+            if (match.MatchDateTime <= DateTime.Now)
+                return false;
+
+            if (string.IsNullOrWhiteSpace(match.Location))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(match.HomeTeam) || string.IsNullOrWhiteSpace(match.AwayTeam) || match.HomeTeam == match.AwayTeam)
+                return false;
+
+            if (match.HomeTeamScore < 0 || match.AwayTeamScore < 0)
+                return false;
+
+            return true;
+        }
+
+        private void DisplayMatch(MatchDetails match)
+        {
+            Console.WriteLine("*******Match Details*******");
+            Console.WriteLine($"Match ID: {match.MatchId}");
+            Console.WriteLine($"Sport: {match.Sport}");
+            Console.WriteLine($"Date & Time: {match.MatchDateTime}");
+            Console.WriteLine($"Venue: {match.Location}");
+            Console.WriteLine($"Home Team: {match.HomeTeam}");
+            Console.WriteLine($"Home Team Score: {match.HomeTeamScore}");
+            Console.WriteLine($"Away Team: {match.AwayTeam}");
+            Console.WriteLine($"Away Team Score: {match.AwayTeamScore}");
+            Console.WriteLine("***************************");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SportsMatchManagementSystem/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace SportsMatchManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchManagement matchManagement = new MatchManagement();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("*********Sports Match Details Management System*********");
+                Console.WriteLine("");
+                Console.WriteLine("1. Add a Match");
+                Console.WriteLine("2. Display All Matches");
+                Console.WriteLine("3. Display Match by ID");
+                Console.WriteLine("4. Update Match Score");
+                Console.WriteLine("5. Remove a Match");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("");
+                Console.WriteLine("********************************************************");
+                Console.Write("Enter your choice: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        // Add a Match
+                        Console.WriteLine("Enter match details:");
+                        MatchDetails newMatch = new MatchDetails();
+                        Console.Write("Match ID: ");
+                        newMatch.MatchId = int.Parse(Console.ReadLine());
+                        Console.Write("Sport: ");
+                        newMatch.Sport = Console.ReadLine();
+                        Console.Write("Date and Time (Format : yyyy-MM-dd HH:mm): ");
+                        newMatch.MatchDateTime = DateTime.ParseExact(Console.ReadLine(), "yyyy-MM-dd HH:mm", null);
+                        Console.Write("Location: ");
+                        newMatch.Location = Console.ReadLine();
+                        Console.Write("Home Team: ");
+                        newMatch.HomeTeam = Console.ReadLine();
+                        Console.Write("Away Team: ");
+                        newMatch.AwayTeam = Console.ReadLine();
+                        Console.Write("Home Team Score: ");
+                        newMatch.HomeTeamScore = int.Parse(Console.ReadLine());
+                        Console.Write("Away Team Score: ");
+                        newMatch.AwayTeamScore = int.Parse(Console.ReadLine());
+
+                        matchManagement.AddMatch(newMatch);
+                        break;
+                    case 2:
+                        // Display All Matches
+                        matchManagement.DisplayAllMatches();
+                        break;
+                    case 3:
+                        // Display Match by ID
+                        Console.Write("Enter Match ID: ");
+                        int matchId = int.Parse(Console.ReadLine());
+                        matchManagement.DisplayMatchById(matchId);
+                        break;
+                    case 4:
+                        // Update Match Score
+                        Console.Write("Enter Match ID: ");
+                        int updateMatchId = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Home Team Score: ");
+                        int homeTeamScore = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Away Team Score: ");
+                        int awayTeamScore = int.Parse(Console.ReadLine());
+                        matchManagement.UpdateMatchScore(updateMatchId, homeTeamScore, awayTeamScore);
+                        break;
+                    case 5:
+                        // Remove a Match
+                        Console.Write("Enter Match ID to remove: ");
+                        int removeMatchId = int.Parse(Console.ReadLine());
+                        matchManagement.RemoveMatch(removeMatchId);
+                        break;
+                    case 6:
+                        // Exit
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
